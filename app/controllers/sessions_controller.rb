5efa9378613c88de7c3dd721601aac53495125f1
jeynation <jeyonglee@gmail.com>
2015-10-22 00:44:37 +0000

    Debugged issues with emails field reseting when login errors out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,8 +10,9 @@ class SessionsController < ApplicationController
       session[:user_id] = auth_user.id
       redirect_to root_path
     else
-      flash[:error] = "Invalid email and password combination."
-      redirect_to login_path
+      flash.now[:error] = "Invalid email and password combination."
+      params[:password] = ""
+      render "new"
     end
   end
 
