fe7485d6bc47a3ad44f6d64f45801f3197456982
jeynation <jeyonglee@gmail.com>
2015-11-11 04:51:54 +0000

    update unit test for weights model

--- test/models/weight_test.rb
+++ test/models/weight_test.rb
@@ -8,6 +8,27 @@ class WeightTest < ActiveSupport::TestCase
     weight.Units = "xx"
     weight.Date = "2015-11-07 00:00:00"
     assert_not weight.save
+    
+    weight.Units = "lb"
+    assert_not_nil weight.save
+    
+    weight.Units = "kb"
+    assert_not_nil weight.save
   end
+  
+  test "weight should be greater than or eqaul to zero" do
+    weight = Weight.new
+    weight.Units = "lb"
+    weight.Date = "2015-11-07 00:00:00"
+    weight.Weight = -1
+    assert_not weight.save
+    
+    weight.Weight = 0
+    assert_not_nil weight.save
+    
+    weight.Weight = 1
+    assert_not_nil weight.save
+  end
+
   
 end
